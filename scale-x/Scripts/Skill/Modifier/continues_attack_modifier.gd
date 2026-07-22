5d81e10467ac1f80236c773eb1fc4b2fbcb8f1b9
Stephen Ho <stephen.ho.232@gmail.com>
2026-07-22 23:02:03 +0700

    Update continues_attack_modifier.gd

--- scale-x/Scripts/Skill/Modifier/continues_attack_modifier.gd
+++ scale-x/Scripts/Skill/Modifier/continues_attack_modifier.gd
@@ -9,24 +9,22 @@ func _init():
 func get_modifier_name() ->String: return "Continues Attack"
 
 func get_modifier_description() -> String:
-	return "+1 attack after each round. Have 65% chance to be destroyed on getting attacked"
+	return "+1 attack after each round. Have 45% chance to be destroyed after round ended"
 
 
 func apply(cell:PlayerCellGridUi):
 	super.apply(cell)
 	EventBus.on_fight_end.connect(_on_fight_end)
-	EventBus.on_enemy_attack.connect(_on_enemy_attack)
-	
+
 	
 func remove():
 	super.remove()
 	EventBus.on_fight_end.disconnect(_on_fight_end)
-	EventBus.on_enemy_attack.disconnect(_on_enemy_attack)
 	_owner_cell.assigned_item.attack -= _bonus_attack
 	_bonus_attack = 0
 	
 	
-func _on_fight_end():
+func _on_fight_end() -> void:
 	_bonus_attack += 1
 	_owner_cell.assigned_item.attack += 1
 	(IngameDataManager.text_manager as TextManager).show_text("+1 Atk", _owner_cell.global_position)	
@@ -34,8 +32,10 @@ func _on_fight_end():
 	EventBus.on_recalculate_player_stat.emit()
 	await _owner_cell.play_bounce_tween()	
 	
-	
-func _on_enemy_attack() -> void:
 	var chance:float = randf_range(0, 100)
 	if chance > 65: return
 	_owner_cell.unassign_item()
+	EventBus.on_recalculate_player_stat.emit()
+	(IngameDataManager.text_manager as TextManager).show_text("Destroyed", _owner_cell.global_position, Color.DARK_GRAY)	
+	AudioManager.play_sfx(SfxContainer.SfxID.NEGATIVE_MODIFIER)	
+	
