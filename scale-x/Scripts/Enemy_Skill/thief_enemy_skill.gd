208728ebefde24218b34a71efc9daad78951d838
Stephen Ho <stephen.ho.232@gmail.com>
2026-07-23 08:17:40 +0700

    Nerf thief skill

--- scale-x/Scripts/Enemy_Skill/thief_enemy_skill.gd
+++ scale-x/Scripts/Enemy_Skill/thief_enemy_skill.gd
@@ -2,11 +2,11 @@ class_name ThiefEnemySkill extends EnemySkill
 
 func get_skill_name() ->String: return "Thief"
 
-func get_skill_description() ->String: return "Steal random 2-8 coins"
+func get_skill_description() ->String: return "Steal random 0-8 coins"
 
 
 func apply() -> void:
 	super.apply()
-	EventBus.on_coin_change.emit(randi_range(2, 8) * -1)
+	EventBus.on_coin_change.emit(randi_range(0, 8) * -1)
 	(IngameDataManager.text_manager as TextManager).show_text("Stolen", _enemy_controller.enemy_pivot.global_position, Color.YELLOW)
 	AudioManager.play_sfx(SfxContainer.SfxID.NEGATIVE_MODIFIER)
